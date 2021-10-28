371ea7a2dd2caa1846e9c0bf07a5ec61dd4b05b4
Max the Human <ayohmei@gmail.com>
2021-10-28 18:42:31 +0200

    nya

--- src/contextapi/actions/projects_actions.ts
+++ src/contextapi/actions/projects_actions.ts
@@ -1,6 +1,20 @@
-import { projects } from '../../pages/Projects/mock-projects'
-import { IProjectsAction } from '../reducer/projects_reducer'
+import { collection, getDocs, query } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { IProjectsAction, IProjectsPayload } from '../reducer/projects_reducer'
 
-export const get_projects = (dispatch: React.Dispatch<IProjectsAction>) => {
-  dispatch({ type: 'GET_PROJECTS', payload: projects })
+export const get_projects = async (dispatch: React.Dispatch<IProjectsAction>) => {
+  const q = query(collection(db, 'projects'))
+  const payload = [] as IProjectsPayload[]
+
+  const querySnapshot = await getDocs(q)
+  querySnapshot.forEach((doc) => {
+    const project = doc.data() as IProjectsPayload
+    payload.push(project)
+  })
+
+  payload.sort((a, b) => {
+    return a.page - b.page
+  })
+
+  dispatch({ type: 'GET_PROJECTS', payload })
 }
---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBsQ_TqLg8t6ct8CeH7KvyJ5zApVPMDh3E',
+  authDomain: 'maxim-yourich.firebaseapp.com',
+  projectId: 'maxim-yourich',
+  storageBucket: 'maxim-yourich.appspot.com',
+  messagingSenderId: '339856072251',
+  appId: '1:339856072251:web:40d60b077062274693061e',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
